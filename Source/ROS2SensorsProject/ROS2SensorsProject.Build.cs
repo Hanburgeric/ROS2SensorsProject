66b27e2e579d98a6888f8b05348abd6f50f996bf
Hanu <hanweonjin415@gmail.com>
2025-08-02 15:50:40 +0900

    Added rclUE and CineCameraSceneCapture as modules

--- Source/ROS2SensorsProject/ROS2SensorsProject.Build.cs
+++ Source/ROS2SensorsProject/ROS2SensorsProject.Build.cs
@@ -1,5 +1,3 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
 using UnrealBuildTool;
 
 public class ROS2SensorsProject : ModuleRules
@@ -8,6 +6,10 @@ public class ROS2SensorsProject : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "ChaosVehicles", "PhysicsCore" });
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "ChaosVehicles", "PhysicsCore",
+			"CinematicCamera", "CineCameraSceneCapture",
+			"rclUE"
+		});
 	}
 }
